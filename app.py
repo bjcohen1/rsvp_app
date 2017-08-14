e805b97bb0815c62e7f1a51a5d9e341186a15cc5
Barry <bjcohen3@gmail.com>
2017-08-13 21:50:56 -0400

    feat: Send text messages to all registered users

--- app.py
+++ app.py
@@ -51,17 +51,19 @@ def welcome():
 #sends text message to "to" with body "body"
 @app.route("/broadcast", methods=['GET','POST'])
 def broadcast():
-    message = client.api.account.messages.create(to="XXXX",
-                                                 from_="XXXX",
-                                                 body="test")
+    user_numbers = session.query(User).all()
+    for number in user_numbers:
+        message = client.api.account.messages.create(to=number,
+                                                     from_="XXXX",
+                                                     body="test")
     return str(message)
 
 #add callers section to respond to personalize response to incoming text
-callers = {
-    "num1" : "name_1",
-    "num2" : "name_2",
-    "num3" : "name_3"
-}
+#callers = {
+#    "num1" : "name_1",
+#    "num2" : "name_2",
+#    "num3" : "name_3"
+#}
 
 @app.route("/sms_answer", methods=['GET','POST'])
 def sms_respond():
